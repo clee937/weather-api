5071f25e20512218aeff3129adb3e3c06c0bf0dc
Cheryl Lee <clee0937@gmail.com>
2024-06-10 15:05:19 +0100

    added types for weather info and user location

---
+++ src/types/WeatherInfo.ts
@@ -0,0 +1,11 @@
+export type WeatherInfo = {
+  location: string;
+  country: string;
+  temperature: number;
+  feelsLike: number;
+  condition: string;
+  wind: number;
+  time: string;
+  sunrise: string;
+  sunset: string;
+};
