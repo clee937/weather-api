b082ba6c7862b0dc441af3d963ad657917b95326
Cheryl Lee <clee0937@gmail.com>
2024-06-11 15:36:27 +0100

    removed unsused variables

--- src/types/WeatherInfo.ts
+++ src/types/WeatherInfo.ts
@@ -1,11 +1,9 @@
 export type WeatherInfo = {
   location: string;
-  country: string;
   temperature: number;
   feelsLike: number;
   condition: string;
   wind: number;
-  time: string;
   sunrise: string;
   sunset: string;
 };
