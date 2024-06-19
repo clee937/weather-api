a992f0c19170d3908043731e9197f12dc3f4f142
Cheryl Lee <clee0937@gmail.com>
2024-06-19 21:21:37 +0100

    added region and country to type

--- src/types/WeatherInfo.ts
+++ src/types/WeatherInfo.ts
@@ -1,8 +1,11 @@
 export type WeatherInfo = {
   location: string;
+  region: string;
+  country: string;
   temperature: number;
   feelsLike: number;
   condition: string;
+  code: number;
   wind: number;
   sunrise: string;
   sunset: string;
