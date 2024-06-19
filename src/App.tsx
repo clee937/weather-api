676a8e018619b13a7f96746a1004e1a15dc38ceb
Cheryl Lee <clee0937@gmail.com>
2024-06-19 21:22:48 +0100

    added functionality and styles for todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,48 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
-import WeatherDisplay from "./WeatherDisplay/WeatherDisplay";
-// import { Coordinates } from "./types/UserLocation";
+import { useEffect, useState } from "react";
+import WeatherDisplay from "./Components/WeatherDisplay/WeatherDisplay";
 import { WeatherInfo } from "./types/WeatherInfo";
 import sunrise from "./assets/images/sunrise.png";
 import sun from "./assets/images/sun.png";
 import moon from "./assets/images/moon.png";
-import Greeting from "./Greeting/Greeting";
+import Greeting from "./Components/Greeting/Greeting";
+import { icons } from "./data/icons";
+import LoadingSpinner from "./Components/LoadingSpinner/LoadingSpinner";
+import TodoForm from "./Components/TodoForm/TodoForm";
+import { Todo } from "./types/Todo";
+import TodoList from "./Components/TodoList/TodoList";
 
 const getGreeting = (hour: number) => {
-  if (hour < 12) {
-    return "Good Morning";
-  }
-  if (hour < 18) {
-    return "Good Afternoon";
-  }
-  return "Good Evening";
+  if (hour < 12) return "Good Morning!";
+  if (hour < 22) return "Good Afternoon!";
+  return "Good Evening!";
 };
 
 const getGreetingImage = (hour: number) => {
-  if (hour < 12) {
-    return sunrise;
-  }
-  if (hour < 18) {
-    return sun;
-  }
+  if (hour < 12) return sunrise;
+  if (hour < 22) return sun;
   return moon;
 };
 
 function App() {
-  // const [userLocation, setUserLocation] = useState<Coordinates | null>(null);
-  const [weatherInfo, setWeatherInfo] = useState<WeatherInfo>({
-    location: "",
-    temperature: 0,
-    feelsLike: 0,
-    condition: "",
-    wind: 0,
-    sunrise: "",
-    sunset: "",
+  const [weatherInfo, setWeatherInfo] = useState<WeatherInfo | null>(null);
+
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const localStoredTodos = localStorage.getItem("todos");
+    if (localStoredTodos == null) return [];
+
+    return JSON.parse(localStoredTodos);
   });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const getUserLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-
-          // setUserLocation({ latitude, longitude });
           getWeather(latitude, longitude);
         },
         (error) => {
@@ -58,16 +54,19 @@ function App() {
     }
   };
 
+  const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
   const getWeather = async (lat: number, long: number) => {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=a2bcc37b2425486998b155054240506&q=${lat},${long}&days=1&aqi=no&alerts=no`;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${lat},${long}&days=1&aqi=no&alerts=no`;
+
     const result = await fetch(url);
     const data = await result.json();
     console.log(data);
 
     const {
-      location: { name },
+      location: { name, region, country },
       current: {
-        condition: { text: condition } = "Loading…",
+        condition: { text: condition, code } = "Loading…",
         temp_c,
         feelslike_c,
         wind_mph,
@@ -81,14 +80,20 @@ function App() {
       },
     } = data;
 
+    const sunriseTime = sunrise.replace(/^0+/, "");
+    const sunsetTime = sunset.replace(/^0+/, "");
+
     setWeatherInfo({
       location: name,
+      region: region,
+      country: country,
       temperature: temp_c,
       feelsLike: feelslike_c,
       condition: condition,
+      code: code,
       wind: wind_mph,
-      sunrise: sunrise,
-      sunset: sunset,
+      sunrise: sunriseTime,
+      sunset: sunsetTime,
     });
   };
 
@@ -96,26 +101,62 @@ function App() {
     getUserLocation();
   }, []);
 
-  const date = new Date();
-
   const currentHour = new Date().getHours();
-  console.log({ date });
   const greeting = getGreeting(currentHour);
   const greetingImage = getGreetingImage(currentHour);
 
+  const getConditionIcon = (code: number, hour: number) => {
+    if (hour > 4 && hour < 22) {
+      for (const info in icons) {
+        if (code === icons[info].code) {
+          return icons[info].icon;
+        }
+      }
+    } else {
+      for (const info in icons) {
+        if (code === icons[info].code) {
+          return icons[info].icon_night;
+        }
+      }
+    }
+  };
+
+  if (!weatherInfo) return <LoadingSpinner />;
+
+  const addTodo = (todo: string) => {
+    const newTodos = [...todos, { todo, isComplete: false }];
+    setTodos(newTodos);
+  };
+
+  const conditionIcon = getConditionIcon(weatherInfo.code, currentHour);
+
+  const toggleTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].isComplete = !newTodos[index].isComplete;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number) => {
+    const newTodos = todos.filter((_: Todo, i: number) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
-      {/* <img src={greetingImage} alt="current time icon" />
-      <h1>{greeting}</h1>
-      {userLocation && (
-        <div>
-          <p>Latitude: {userLocation.latitude}</p>
-          <p>Latitude: {userLocation.longitude}</p>
-        </div>
-      )}
-      <h1></h1> */}
-      <Greeting greeting={greeting} greetingImage={greetingImage} />
-      <WeatherDisplay weather={weatherInfo} />
+      <div className="app__weather">
+        <Greeting greeting={greeting} greetingImage={greetingImage} />
+        <WeatherDisplay weather={weatherInfo} icon={conditionIcon || sun} />
+      </div>
+      <div className="app__todos">
+        <Greeting greeting="Todo List" />
+        <TodoForm addTodo={addTodo} />
+
+        <TodoList
+          todos={todos}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      </div>
     </div>
   );
 }
