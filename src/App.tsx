66c0a6e446f94372f43adac99c2a5f7704940e14
Cheryl Lee <clee0937@gmail.com>
2024-06-19 21:45:12 +0100

    amended getGreeting function

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,13 @@ import TodoList from "./Components/TodoList/TodoList";
 
 const getGreeting = (hour: number) => {
   if (hour < 12) return "Good Morning!";
-  if (hour < 22) return "Good Afternoon!";
+  if (hour < 18) return "Good Afternoon!";
   return "Good Evening!";
 };
 
 const getGreetingImage = (hour: number) => {
   if (hour < 12) return sunrise;
-  if (hour < 22) return sun;
+  if (hour < 18) return sun;
   return moon;
 };
 
